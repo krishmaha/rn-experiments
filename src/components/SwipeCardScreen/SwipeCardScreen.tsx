ec022d231763286a29db9f61b0515b5d700bc36a
Krish Maha <krish@mahas.net>
2024-07-24 11:22:30 +0100

    Add swipable cards

--- src/components/SwipeCardScreen/SwipeCardScreen.tsx
+++ src/components/SwipeCardScreen/SwipeCardScreen.tsx
@@ -1,6 +1,45 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View, Button, StyleSheet } from 'react-native';
+import Swiper from 'react-native-deck-swiper';
 
 export default function SwipeCardScreen() {
-  return <Text>Hi i am swipe card screen</Text>
+  return (
+    <View style={styles.container}>
+    <Swiper
+        cards={['DO', 'MORE', 'OF', 'WHAT', 'MAKES', 'YOU', 'HAPPY']}
+        renderCard={(card) => {
+          return (
+            <View style={styles.card}>
+              <Text style={styles.text}>{card}</Text>
+            </View>
+          )
+        }}
+        onSwiped={(cardIndex) => {console.log(cardIndex)}}
+        onSwipedAll={() => {console.log('onSwipedAll')}}
+        cardIndex={0}
+        backgroundColor={'#4FD0E9'}
+        stackSize= {3}>
+      </Swiper>
+    </View>
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#F5FCFF"
+  },
+  card: {
+    flex: 1,
+    borderRadius: 4,
+    borderWidth: 2,
+    borderColor: "#E8E8E8",
+    justifyContent: "center",
+    backgroundColor: "white"
+  },
+  text: {
+    textAlign: "center",
+    fontSize: 50,
+    backgroundColor: "transparent"
+  }
+});
